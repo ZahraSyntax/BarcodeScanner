27e8aca60af032bb2e97c65ba8e1da4cfdbcfd65
Zahra Bazzi <ms.hosna2003@gmail.com>
2024-11-04 15:33:48 +0330

    Return scan result with ZXing

--- app/src/main/java/befoys/bazzi/brscanner2/MainActivity.kt
+++ app/src/main/java/befoys/bazzi/brscanner2/MainActivity.kt
@@ -3,10 +3,11 @@ package befoys.bazzi.brscanner2
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.zxing.integration.android.IntentIntegrator
+import com.google.zxing.integration.android.IntentResult
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,9 +24,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun startBarcodeScanner() {
         val integrator = IntentIntegrator(this)
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES)
+        integrator.setPrompt("اسکن بارکد یا QR Code را شروع کنید")
         integrator.setCameraId(0)
+        integrator.setBeepEnabled(true)
+        integrator.setBarcodeImageEnabled(true)
         integrator.initiateScan()
     }
 
-
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result: IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "اسکن لغو شد", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "نتیجه: ${result.contents}", Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
 }
